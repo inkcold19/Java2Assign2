e393758290e74c0be3da990306cb172aeaf2a6e1
inkcold19 <113008427+inkcold19@users.noreply.github.com>
2022-11-24 13:15:41 +0800

    Update Register.java

--- Register.java
+++ Register.java
@@ -1,4 +1,5 @@
 
+
 import bean.Person;
 import bean.PersonDAO;
 import javafx.application.Application;
@@ -8,21 +9,21 @@ import java.util.Scanner;
 
 public class register extends Application {
 
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
+  public static void main(String[] args) {
+    Application.launch(args);
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        System.out.print("请输入用户名：");
-        Scanner scan = new Scanner(System.in);
-        String read = scan.nextLine();
-        System.out.print("请输入密码：");
-        scan = new Scanner(System.in);
-        String read1 = scan.nextLine();
-        PersonDAO dao = new PersonDAO();
-        Person person = dao.reg(read, read1);
-        System.out.print("注册成功");
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    System.out.print("请输入用户名：");
+    Scanner scan = new Scanner(System.in);
+    String read = scan.nextLine();
+    System.out.print("请输入密码：");
+    scan = new Scanner(System.in);
+    String read1 = scan.nextLine();
+    PersonDAO dao = new PersonDAO();
+    Person person = dao.reg(read, read1);
+    System.out.print("注册成功");
 
-    }
+  }
 }
