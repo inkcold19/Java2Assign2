62e732e116035f38727087dbdefd769d55685e59
inkcold19 <113008427+inkcold19@users.noreply.github.com>
2022-11-24 13:09:30 +0800

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,41 +1,42 @@
-package bean;
+
 
 public class Person {
-	private int id;
-	private String username;
-	private String password;
-	private String role;
-	
-	public int getId() {
-		return id;
-	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
+  private int id;
+  private String username;
+  private String password;
+  private String role;
 
-	public String getUsername() {
-		return username;
-	}
+  public int getId() {
+    return id;
+  }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+  public void setId(int id) {
+    this.id = id;
+  }
 
-	public String getPassword() {
-		return password;
-	}
+  public String getUsername() {
+    return username;
+  }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-	public String getRole() {
-		return role;
-	}
+  public String getPassword() {
+    return password;
+  }
 
-	public void setRole(String role) {
-		this.role = role;
-	}
-	
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
 }
