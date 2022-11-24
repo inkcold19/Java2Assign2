5e2187debd8623fb589fe723b400160f7069cba8
inkcold19 <113008427+inkcold19@users.noreply.github.com>
2022-11-24 13:17:26 +0800

    Create Server_Start.java

---
+++ Server_Start.java
@@ -0,0 +1,24 @@
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server_Start {
+
+  public static void main(String[] args) throws IOException {
+    ServerSocket serverSocket = new ServerSocket(8087);
+    System.out.println("服务器启动");
+        try {
+          while (true) {
+           Socket socket = serverSocket.accept();
+        Server st = new Server(socket);
+        st.start();
+
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
+
