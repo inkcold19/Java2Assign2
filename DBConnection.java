8a95cd18dd9444a93fd2b6201d43dcf6b92fff9c
inkcold19 <113008427+inkcold19@users.noreply.github.com>
2022-11-24 13:18:24 +0800

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -1,27 +1,30 @@
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	private static Connection conn = null;
-	private static final String CLASSNAME = "com.mysql.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://mysql.sqlpub.com:3306/dnld66?useSSL=false";
-	private static final String USER = "dnld66";
-	private static final String PASSWORD = "9729c63a353adb95";
-	
-	public static Connection getConnection() {
-		try {
-			if (conn != null && !conn.isClosed())
+
+  private static Connection conn = null;
+  private static final String CLASSNAME = "com.mysql.jdbc.Driver";
+  private static final String URL = "jdbc:mysql://mysql.sqlpub.com:3306/dnld66?useSSL=false";
+  private static final String USER = "dnld66";
+  private static final String PASSWORD = "9729c63a353adb95";
+
+  public static Connection getConnection() {
+    try {
+			if (conn != null && !conn.isClosed()) {
 				return conn;
-			Class.forName(CLASSNAME);
-			conn = DriverManager.getConnection(URL, USER, PASSWORD);
-			return conn;
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+			}
+      Class.forName(CLASSNAME);
+      conn = DriverManager.getConnection(URL, USER, PASSWORD);
+      return conn;
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
 }
